32afe72a805b78eda21a2a47d6eca166a09a1ab8
egorbarinov <egor.barinov@gmail.com>
2021-02-09 10:46:44 +0300

    code from the homework assignment for the lesson03

---
+++ src/main/java/com/egorbarinov/lesson03/MyAtomicCounter.java
@@ -0,0 +1,30 @@
+package com.egorbarinov.lesson03;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class MyAtomicCounter {
+
+    public static AtomicLong count = new AtomicLong(0);
+    public static final Object object = new Object();
+
+    public static void main(String[] args) throws InterruptedException {
+
+        synchronized (object) {
+
+            for (int i = 0; i < 30; i++) {
+                new Thread(() -> {
+                    synchronized (object) {
+                        count.incrementAndGet();
+                        System.out.println(count.get());
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+            }
+        }
+
+    }
+}
